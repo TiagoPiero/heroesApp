e48b04633bb39a0bb89c3de281474aae581a8317
Tiago Pieroni <tepieroni@gmail.com>
2024-03-08 19:31:32 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import ('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import ('./heroes/heroes.module').then( m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'     // esto se hace para que el path sea exactamente vacio. Si no se pone, puee entrar en cualq ruta ya que en cualquier path siempre hay un string vacio antes de la primera letra
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
