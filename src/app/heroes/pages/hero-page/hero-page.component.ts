010e47caa16cae28a7de32aa8300d6239a662984
Tiago Pieroni <tepieroni@gmail.com>
2024-03-12 08:58:46 -0300

    Fin seccion 16

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute, //para leer la ruta especificada
+    private router: Router                   // para redireccionar a heroes/list
+    ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroById( id ) )  //recibo los params, pero necesito solo el id y por eso lo desestructuro.
+      ).subscribe (hero => {
+        if ( !hero ) return this.router.navigate( [ 'heroes/list']);
+        this.hero = hero;
+        return;
+      })
+  }
+
+  goBack(): void {
+   this.router.navigate([ 'heroes/list' ]);
+  }
 
 }
